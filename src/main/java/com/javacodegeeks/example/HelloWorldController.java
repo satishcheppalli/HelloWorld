fc43b2b9cd25e9823b2ed97ea773edca5bf0707a
satishcheppalli <39270018+satishcheppalli@users.noreply.github.com>
2019-10-26 21:36:23 -0400

    Update HelloWorldController.java

--- src/main/java/com/javacodegeeks/example/HelloWorldController.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldController.java
@@ -8,10 +8,10 @@ public class HelloWorldController {
 
 	@GetMapping("/hello")
 	public String sayHello() throws InterruptedException {
-		for(int i=0; i<=10000;i++){
+		/*for(int i=0; i<=10000;i++){
 			System.out.println(i);
-		}
-		Thread.sleep(60 * 1000);
+		}*/
+		Thread.sleep(5 * 1000);
 		return "Hello world with rolling updates...";
 	}
 }
